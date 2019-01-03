e717df5555f39fede6012353e2840bc1b94e0145
Ruslan Uchan <ruslanuchan97@gmail.com>
2019-01-04 01:35:27 +0700

    Start!

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React, { Component } from "react"
 import "./App.css"
 
-import LineChart from "./components/LineChart"
-import BarChart from "./components/BarChart"
 import ScatterChart from "./components/ScatterChart"
-import ScatterChartjs from "./components/ScatterChartjs"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LineChart />
-        <BarChart />
         <ScatterChart />
-        <ScatterChartjs />
       </div>
     )
   }
