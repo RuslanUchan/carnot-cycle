a28bd7084b72506a331a1d9ee905266631fbe093
Ruslan Uchan <ruslanuchan97@gmail.com>
2019-01-07 12:37:19 +0700

    Some visualization done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,24 @@ class App extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      initialVolume: [2, 8, 9, 3],
+      initialPressure: [8, 6, 2, 4],
+      modifier: [[1.2, -0.4], [0.2, -0.8], [-1.2, 0.4], [-0.2, 0.8]]
+    }
+  }
+
+  PVDiagramModifier = (x, y, xMod, yMod) => {
+    const seriesData = [[x, y]]
+
+    for (let i = 0; i < 5; i++) {
+      x += xMod
+      y += yMod
+
+      seriesData.push([x, y])
+    }
+
+    return seriesData
   }
 
   render() {
@@ -18,7 +35,10 @@ class App extends Component {
         <h1 style={{ marginBottom: "20px" }}>Carnot Cycle</h1>
         <Calculator />
         <Container style={{ marginTop: "10px" }}>
-          <ScatterChart />
+          <ScatterChart
+            PVDiagramModifier={this.PVDiagramModifier}
+            systemVariables={this.state}
+          />
         </Container>
         <Button color="primary" onClick={this.handleVisualize} size="lg">
           Visualize
